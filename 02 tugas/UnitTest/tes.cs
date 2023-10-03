e181a3bcd3aa5081d523bc0e0ddb439bb38d6fd1
Packooo <you@example.com>
2023-10-03 20:12:17 +0700

    tugas 2- selesai

---
+++ 02 tugas/UnitTest/tes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+
+namespace UnitTest
+{
+    [TestFixture]
+
+    public class tes
+    {
+        private Day day;
+        private User user;
+
+        [SetUp]
+        public void Init()
+        {
+            day = new Day();
+            user = new User();
+        }
+       
+
+        [Test]
+        public void DayTest()
+        {
+            Assert.AreEqual("Senin", day.NameOfDay(1));
+        }
+
+        [Test]
+        public void UserTest()
+        {
+            Assert.AreEqual(true, user.IsValidUser("admin","admin"));
+        }
+    }
+}
