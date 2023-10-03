7d7ccc78381fa7a98f0544ff18dc77f01ca73045
Packooo <you@example.com>
2023-10-03 19:29:50 +0700

    Add project files.

---
+++ 01 latihan/CalculatorConsoleTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CalculatorMachine;
+
+namespace CalculatorConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Demo Unit Menggunakan Console App";
+            Calculator cal = new Calculator();
+
+            Console.WriteLine("Hasil Penambahan 2 + 3 = {0}", cal.Penambahan(2, 3));
+            Console.WriteLine("Hasil Pengurangan 7 - 3 = {0}", cal.Pengurangan(7, 3));
+            Console.WriteLine("Hasil Perkalian 5 x 2 = {0}", cal.Perkalian(5, 2));
+            Console.WriteLine("Hasil Pembagian 6 : 2 = {0}", cal.Pembagian(6, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01 latihan/CalculatorUnitTest/CalculatorTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CalculatorMachine;
+
+using NUnit.Framework;
+
+namespace CalculatorUnitTest
+{
+    [TestFixture] //test fixture
+    class CalculatorTest
+    {
+        private Calculator calculator; // membuat object
+
+        [SetUp] //settup inisialisasi
+        public void Init()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void PenambahanTest()
+        {
+            Assert.AreEqual(5, calculator.Penambahan(2, 3)); //mengetes fungsi penambahan
+        }
+        [Test]
+        public void PenguranganTest()
+        {
+            Assert.AreEqual(4, calculator.Pengurangan(7, 3)); //mengetes pengurangan
+        }
+        [Test]
+        public void PerkalianTest()
+        {
+            Assert.AreEqual(10, calculator.Perkalian(5, 2)); //mengetes perkalian
+        }
+        [Test]
+        public void PembagianTest()
+        {
+            Assert.AreEqual(3, calculator.Pembagian(6, 2)); //mengetes pembagian
+        }
+    }
+}
